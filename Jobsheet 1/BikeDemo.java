88a32af4c824572b9b105662bde680a5d30ef1c9
Fizzuz <mhafizgpr@gmail.com>
2026-08-28 09:12:33 +0700

    Percobaan 2

--- Jobsheet 1/BikeDemo.java
+++ Jobsheet 1/BikeDemo.java
@@ -2,6 +2,7 @@ public class BikeDemo {
     public static void main(String[] args) {
         Bike MountainBike1 = new Bike();
         Bike MountainBike2 = new Bike();
+        RoadBike roadBike1 = new RoadBike();
 
         MountainBike1.setBrand("Trek");
         MountainBike1.speedAcceleration(10);
@@ -12,5 +13,11 @@ public class BikeDemo {
         MountainBike2.speedAcceleration(20);
         MountainBike2.gearChanges(3);
         MountainBike2.printInfo();
+
+        roadBike1.setBrand("Specialized");
+        roadBike1.setTireWidth(25);
+        roadBike1.speedAcceleration(15);
+        roadBike1.gearChanges(4);
+        roadBike1.printInfo();
     }
 }
